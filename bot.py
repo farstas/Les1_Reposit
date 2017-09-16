7013d51d162a5b07fc94a07e04db3e10d3251eb8
farstas <farstas@mail.ru>
2017-09-16 12:11:23 +0300

    Обновил бота шаг6

--- bot.py
+++ bot.py
@@ -4,16 +4,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-                    )
+					level=logging.INFO,
+					filename='bot.log'
+					)
 
 
 def start_bot(bot, update):
-	mytext = '''Привет {}!
-
-Я протой бот и понимаю только команду /start
-	'''.format(update.message.chat.first_name)
+	mytext = 'Привет {}! Я протой бот и понимаю только команду {}'.format(update.message.chat.first_name,'/start')
+	logging.info('Пользователь {} нажал /start'.format(update.message.chat.username))
 	update.message.reply_text(mytext)
 
 def chat(bot, update):
