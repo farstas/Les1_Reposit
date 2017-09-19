85c115ba07c834f39e301435110b7981656d47fb
farstas <farstas@mail.ru>
2017-09-19 08:39:45 +0300

    Дорабатывал калькулятор для бота

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+dict = {}
 import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -10,20 +13,60 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 
 
 def start_bot(bot, update):
-	mytext = 'Привет {}! Я протой бот и понимаю только команду {}'.format(update.message.chat.first_name,'/start')
+	mytext = 'Привет {}! Я простой бот и понимаю только команду {}'.format(update.message.chat.first_name,'/start')
 	logging.info('Пользователь {} нажал /start'.format(update.message.chat.username))
 	update.message.reply_text(mytext)
 
 def chat(bot, update):
 	text = update.message.text
+	chat_id = update.message.chat.id
 	logging.info(text)
-	update.message.reply_text(text)
+
+	if text in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/']:
+		 if dict.get(chat_id) == None:
+		 	dict[chat_id] = text
+		 else:
+		 	dict[chat_id] += text
+		 	
+	elif text == '=':
+		clc = dict.get(chat_id)
+		print(clc)
+		#работа функции + del dict[chat_id]
+
+	elif text == 'Esc':
+		clear_keyboards(bot,chat_id)
+
+	else:
+		update.message.reply_text(text)
+
+def wordcount_bot(bot, update, args):
+	text= update.message.text
+	print(text)
+	user_text = text.split()
+	user_text = [item for item in user_text if item not in ['?','!', '.', '.', ',', ':', ';', '*', '"']]
+	
+	update.message.reply_text(len(user_text)-1)
+	print(len(user_text)-1)
+
+
+def calculator_bot(bot, update, args):
+	print(update.message.chat.id)
+	custom_keyboard = [['1', '2', '3', '/'], ['4', '5', '6', '*'], ['7', '8', '9', '-'], ['Esc', '0', '+', '=']]
+	reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+	bot.send_message(chat_id = update.message.chat.id, text = "Используйте кнопки калькулятора для расчета!", reply_markup = reply_markup)
+
+def clear_keyboards(bot, userssss_id):
+	reply_markup = telegram.ReplyKeyboardRemove(remove_keyboard=True)
+	bot.send_message(chat_id = userssss_id, text = "Наберите новую команду: /start /calculator /wordcount", reply_markup = reply_markup)
+	
 
 def main():
 	updtr = Updater(settings.TELEGRAM_API_KEY)
 	
 	updtr.dispatcher.add_handler(CommandHandler('start', start_bot))
 	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+	updtr.dispatcher.add_handler(CommandHandler('wordcount', wordcount_bot, pass_args=True))
+	updtr.dispatcher.add_handler(CommandHandler('calculator', calculator_bot, pass_args=True))
 
 	updtr.start_polling()
 	updtr.idle()
@@ -33,6 +76,4 @@ def main():
 if __name__=='__main__':
 	logging.info('Bot started')
 	main()
-
-
 
---
+++ calculator.py
@@ -0,0 +1,24 @@
+def calculator_bot(bot, update, args):
+    text= update.message.text
+    text = text.replace('+', ' + ')
+    text = text.replace('-', ' - ')
+    text = text.replace('*', ' * ')
+    text = text.replace('/', ' / ')
+    user_text = text.split()
+    user_text = user_text[2:]
+    print(user_text)
+    for items in user_text:
+            if items == '*':
+                index = user_text.index(items)
+                user_text[index] = int(user_text[index - 1]) * int(user_text[index + 1])
+                user_text[index - 1] = '?'
+                user_text[index + 1] = '?'
+                print(user_text)
+    
+    for items in user_text:
+            if items == '/':
+                index = user_text.index(items)
+                user_text[index] = int(user_text[index - 1]) / int(user_text[index + 1])
+                del user_text[index - 1]
+                del user_text[index]
+                print(user_text[index])
